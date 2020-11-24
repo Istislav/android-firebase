7b746f625359915fb545a5d4cd51e0ef4e3fe13e
CyberUnit <cyberunit@gmail.com>
2020-11-24 12:00:28 +0300

    preparing for firefase authentication

--- app/src/main/java/ru/istislav/firstfirebaseapp/MainActivity.java
+++ app/src/main/java/ru/istislav/firstfirebaseapp/MainActivity.java
@@ -4,8 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -16,11 +21,24 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseDatabase database;
     private DatabaseReference databaseReference;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "mainActivity";
+
+    private EditText email;
+    private EditText password;
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = (EditText) findViewById(R.id.emailEd);
+        password = (EditText) findViewById(R.id.passEd);
+        loginBtn = (Button) findViewById(R.id.loginButton);
+
+        mAuth = FirebaseAuth.getInstance();
 
         database = FirebaseDatabase.getInstance();
         databaseReference = database.getReference("message");
@@ -39,5 +57,34 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    Log.d(TAG, "user signed in");
+                } else {
+                    Log.d(TAG, "user signed out");
+                }
+
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
