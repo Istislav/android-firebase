1cfa9b1bb10ce1a0e3ce3ad70c25ec4a853bb64d
CyberUnit <cyberunit@gmail.com>
2020-11-24 12:59:11 +0300

    creating new account for firebase

--- app/src/main/java/ru/istislav/firstfirebaseapp/MainActivity.java
+++ app/src/main/java/ru/istislav/firstfirebaseapp/MainActivity.java
@@ -32,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText email;
     private EditText password;
     private Button loginBtn;
+    private Button logoutBtn;
+    private Button createAccBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
         email = (EditText) findViewById(R.id.emailEd);
         password = (EditText) findViewById(R.id.passEd);
         loginBtn = (Button) findViewById(R.id.loginButton);
+        logoutBtn = (Button) findViewById(R.id.logoutButton);
+        createAccBtn = (Button) findViewById(R.id.createAccButton);
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -95,6 +99,39 @@ public class MainActivity extends AppCompatActivity {
                                     Customer customer = new Customer("Misha", "Popkin", emailString, 22);
 
                                     databaseReference.setValue(customer);
+                                }
+                            }
+                        });
+                }
+            }
+        });
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Toast.makeText(MainActivity.this, "You signed out", Toast.LENGTH_SHORT);
+            }
+        });
+
+        createAccBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailString = email.getText().toString();
+                String pwd = password.getText().toString();
+
+                if (!emailString.equals("") && !pwd.equals("")) {
+                    mAuth.createUserWithEmailAndPassword(emailString, pwd)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(MainActivity.this,
+                                            "Failed to create account", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(MainActivity.this,
+                                            "Account created!", Toast.LENGTH_SHORT).show();
+
                                 }
                             }
                         });
